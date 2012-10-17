f4d62aaad3fea43194b48f1e52ad220a5b49af9b
Yitao Li <yl790@nyu.edu>
2012-10-17 00:15:54 -0700

    for some reason expr is not handling negative numbers correctly

--- utils/wget_osm_data.sh
+++ utils/wget_osm_data.sh
@@ -11,18 +11,18 @@ then
 	exit 0
 fi
 
-if [ `expr $1 \> $3` -eq 1 ]
-then
-	echo "Error: the westernmost longtitude cannot be greater than the easternnmost longtitude"
-	echo "Usage: " $0 " <westernmost longtitude> <southernmost latitude> <easternmost longtituderight> <northernmost latitude>" 
-	exit 0
-fi
-
-if [ `expr $2 \> $4` -eq 1 ] 
-then
-	echo "Error: the southernmost latitude cannot be greater than the northernnmost latitude"
-	echo "Usage: " $0 " <westernmost longtitude> <southernmost latitude> <easternmost longtituderight> <northernmost latitude>" 
-	exit 0
-fi
+# if [ `expr $1 \> $3` -eq 1 ]
+# then
+# 	echo "Error: the westernmost longtitude cannot be greater than the easternnmost longtitude"
+# 	echo "Usage: " $0 " <westernmost longtitude> <southernmost latitude> <easternmost longtituderight> <northernmost latitude>" 
+# 	exit 0
+# fi
+# 
+# if [ `expr $2 \> $4` -eq 1 ] 
+# then
+# 	echo "Error: the southernmost latitude cannot be greater than the northernnmost latitude"
+# 	echo "Usage: " $0 " <westernmost longtitude> <southernmost latitude> <easternmost longtituderight> <northernmost latitude>" 
+# 	exit 0
+# fi
 
 wget -dv http://api.openstreetmap.org/api/0.6/map?bbox=$1,$2,$3,$4 2>&1
