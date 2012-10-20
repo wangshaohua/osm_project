5839870f03d76b17a858d0ab7119ec9996a06b34
Yitao Li <yl790@nyu.edu>
2012-10-19 22:56:03 -0700

    more work towards creating vertices and edges

--- src/osm_parse_result.cc
+++ src/osm_parse_result.cc
@@ -1,17 +1,27 @@
 #include "osm_parse_result.h"
 
+long osm_parse_result::find_immediate_predecessor(const long way_id, const size_t index){  //find the vertex in current way that is the immediate predecessor of the current vertex 
+}
+
+
+long osm_parse_result::find_immediate_successor(const long way_id, const size_t index){  //find the vertex in current way that is the immediate successor of the current vertex (if any) 
+}
+
 void osm_parse_result::insert_node_ref(const long node_id, const double lat, const double lon){
 	n.insert(std::pair< long, std::pair<double, double> >(node_id, std::pair<double, double>(lat, lon)));
 }
 
-void osm_parse_result::insert_node_ref(const long node_id, const long way_id){
-	long l_id;
+void osm_parse_result::insert_way_ref(const long node_id, const long way_id){
+	long l_id, _way_id;
+	size_t n_ind, _n_ind, min_n_v, max_n_v;
 	double lat, lon, l_lon, l_lat, dx, dy;
 	std::pair<double, double> *nd, *l_nd; 
-	std::vector<long> *w_v = &(w[way_id]);
+	std::vector<long> *w_nv = &(w[way_id]);
+	std::map<long, size_t> *w_ni;
 	std::map<long, double>::iterator l_itr;
-	if (w_v -> size()){
-		l_id = w_v -> back();
+	std::map<long, size_t>::iterator n_itr;
+	if ((n_ind = w_nv -> size())){
+		l_id = w_nv -> back();
 		nd = &(n[node_id]);
 		l_nd = &(n[l_id]);
 		lat = nd -> first;
@@ -24,6 +34,26 @@ void osm_parse_result::insert_node_ref(const long node_id, const long way_id){
 			l_itr = wl.insert(std::pair<long, double>(way_id, 0.0)).first;
 		}
 		l_itr -> second += sqrt(dy * dy + dx * dx); 
+	}else{
+		v.insert(node_id);    //the starting point of each way must be a vertex
+		wv[way_id].push_back(std::pair<size_t, long>(0, node_id));
 	}
-	w_v -> push_back(node_id);
+	w_nv -> push_back(node_id);   //add the node to current way
+	w_ni = &(wi[node_id]);
+	if (w_ni -> size()){    //current node is contained in another way 
+		if (v.find(node_id) == v.end()){   //current node is not a vertex in the graph yet
+			n_itr = w_ni -> begin();
+			_way_id = n_itr -> first;    //the way id of another way that contains the current node
+			_n_ind = n_itr -> second;    //the index of the current node in another way
+			if (_n_ind){    //find the immediate predessor of current vertex on another way and create an edge
+			min_n_v = 0;
+			max_n_v =  
+			}
+			if (_n_ind < wv[_way_id].size()){    //find the immediate successor of current vertex on another way and create an edge
+			}
+		}else{
+		}
+	}
+	w_ni -> insert(std::pair<long, size_t>(way_id, n_ind));
+
 }
--- src/osm_parse_result.h
+++ src/osm_parse_result.h
@@ -11,16 +11,19 @@
 class osm_parse_result{
 	private:
 		std::map< long, std::pair<double, double> > n;   //map from node id to node latitude / longtitude values
-		std::map< long, std::vector<long> > w;    //map from way id to list of node ids included in that way (in correct order) 
+		std::map< long, std::vector<long> > w;    //map from way id to list of node ids contained in that way (in correct order) 
+		std::map< long, std::map<long, size_t> > wi;  //map from node id to the map of way id and index of node on that way   
 		std::map<long, double> wl;    //map from way id to estimated total length of the way
-		std::map< long, std::map<long, size_t> > wi;  //map from way id to the map of node id and index of node on that way   
-		std::list<long> v;    //the list of vertices in the abstracted version of the map
+		std::map< long, std::vector< std::pair<size_t, long> > > wv;  //map from way id to list of vertices contained in that way 
+		std::set<long> v;    //the list of vertices in the abstracted version of the map
 		std::set< std::pair<long, long> > e;    //the set of edges in the abstracted version of the map
+		long find_immediate_predecessor(const long, const size_t);
+		long find_immediate_successor(const long, const size_t);
 	public:
 		osm_parse_result(void){};
 		~osm_parse_result(void){};
 		void insert_node_ref(const long, const double, const double);  //inserting into n_map 	
-		void insert_node_ref(const long, const long);    //inserting into w_map
+		void insert_way_ref(const long, const long);    //inserting into w_map
 };
 
 #endif
