c82048f8f2d228d2df9ad869d767d3facbfdfb3c
Yitao Li <yl790@uw.edu>
2012-11-15 16:34:13 -0800

    adding intersections in demo

--- ui/viz-create-osm-map.php
+++ ui/viz-create-osm-map.php
@@ -5,22 +5,45 @@
 		<script src="js/d3.v2.js"></script>
 		<script src="js/jquery-1.8.2.min.js"></script>
 		<script>
-			var MAP_POS, X_OFFSET, Y_OFFSET, map, prev_X = null, prev_Y = null, X, Y;	
+			var MAP_POS, MAP_WIDTH, MAP_HEIGHT, X_OFFSET, Y_OFFSET, map, prev_X = null, prev_Y = null, X, Y, lat, lon, node_num = 0, _node_num, way_num = -1, node_exists = false;	
 			$(document).ready(function(){
 				map = d3.select("#map").append("svg:svg").attr("width", "100%").attr("height", "100%");
 				MAP_POS = $("#map").position();
+				MAP_WIDTH = $("#map").width();
+				MAP_HEIGHT = $("#map").height();
 				X_OFFSET = MAP_POS.left;
 				Y_OFFSET = MAP_POS.top;
 				$(document).bind("contextmenu",function(e){
 					return false;
 				}); 
 				$("#map").click(function(event){
-					X = event.pageX - X_OFFSET;
-					Y = event.pageY - Y_OFFSET
-					map.append("svg:circle").attr("cx", X).attr("cy", Y).attr("r", 4).style("fill", "red");
-					if (prev_X != null && prev_Y != null){
-						map.append("svg:line").attr("x1", prev_X).attr("y1", prev_Y).attr("x2", X).attr("y2", Y).style("stroke", "blue").style("stroke-width", 2);  
+					if (!node_exists){   //create a new node
+						X = event.pageX - X_OFFSET;
+						Y = event.pageY - Y_OFFSET;
+						lat = Math.round((-Y * 180.0 / MAP_HEIGHT + 90.0) * 1000000) / 1000000.0;
+						lon = Math.round((X * 180.0 / MAP_WIDTH - 90.0) * 1000000) / 1000000.0;
+						map.append("svg:circle").data([ {node_num: node_num, lat: lat, lon: lon} ]).attr("cx", X).attr("cy", Y).attr("r", 6).style("fill", "red").style("cursor", "pointer").on("click", function(d,i){
+							node_exists = true;
+							lat = d.lat;
+							lon = d.lon;
+							X = d3.select(this).attr("cx");
+							Y = d3.select(this).attr("cy");
+							_node_num = d.node_num;	
+						});
+						_node_num = node_num
+						$("#node-xml").append("&lt;node id=\"" + node_num + "\" lat=\"" + lat + "\" lon=\"" + lon  + "\"/&gt;\n");
+						++node_num;
+					}else{
+						node_exists = false;  //use an existing node
 					}
+					if (prev_X != null && prev_Y != null){
+						map.append("svg:line").attr("x1", prev_X).attr("y1", prev_Y).attr("x2", X).attr("y2", Y).style("stroke", "blue").style("stroke-width", 4); 
+						$("#way-xml").append("\n  ");
+					}else{
+						++way_num;
+						$("#way-xml").append("&lt;way id=\"" + way_num + "\"&gt;\n  ");
+					}
+					$("#way-xml").append("&lt;nd ref=\"" + _node_num + "\"/&gt;")
 					prev_X = X;
 					prev_Y = Y;
 				});
@@ -28,6 +51,7 @@
 					if (event.which == 3){
 						prev_X = null;
 						prev_Y = null;
+						$("#way-xml").append("\n&lt/way&gt;\n")
 					}
 				});
 			});
@@ -49,7 +73,7 @@
 						<table border="1" cellpadding="0" cellspacing="0" style="width: 100%; height: 100%; float: left; clear: left;">
 							<tr style="height:16pt;">
 								<th>OSM Node(s):</th> 
-								<th>OSM Road(s):</th> 
+								<th>OSM Way(s):</th> 
 							</tr>
 							<tr>
 								<td>
@@ -58,8 +82,8 @@
 									</div>
 								</td>
 								<td>
-									<div id="road-div" style="width:100%;height:100%;">
-										<textarea id="road-xml" readonly="true" rows="10" cols="35"></textarea>
+									<div id="way-div" style="width:100%;height:100%;">
+										<textarea id="way-xml" readonly="true" rows="10" cols="35"></textarea>
 									</div>
 								</td>
 							</tr>
